63a5364b91d90dd3f100d70bfc1c46140174aad0
AbyssDelver <muffinarmageddon@gmail.com>
2023-11-03 00:29:06 +0100

    improved organization of directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/liamcavini/laboratorio/lab1
-# Build directory: /Users/liamcavini/laboratorio/lab1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(lab1.t "/Users/liamcavini/laboratorio/lab1/build/lab1.t")
-set_tests_properties(lab1.t PROPERTIES  _BACKTRACE_TRIPLES "/Users/liamcavini/laboratorio/lab1/CMakeLists.txt;34;add_test;/Users/liamcavini/laboratorio/lab1/CMakeLists.txt;0;")
